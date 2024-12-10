f2fa6c825d8a6cd6384a6f1d00a9d2f3bbe575cd
LAViswhat <lav.iswhat@gmail.com>
2024-12-10 22:23:07 +0200

    initial init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>asd</h1>
+    <UiButton class="">HElo</UiButton>
+    <UiButton variant="ghost">
+      <Icon name="line-md:logout" size="32" />
+      <p>GO away</p>
+    </UiButton>
+  </div>
+</template>
