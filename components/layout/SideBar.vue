ce870e325850d753f9410f287696f8fc92722921
LAViswhat <lav.iswhat@gmail.com>
2024-12-21 17:23:19 +0200

    add signOut method

--- components/layout/SideBar.vue
+++ components/layout/SideBar.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import { useAuthStore } from "~/stores/auth";
+
+const authStore = useAuthStore();
+
+const handleSignOut = async () => {
+  await authStore.signOut();
+};
+</script>
 <template>
   <aside class="px-5 py-8 bg-secondary h-full w-full relative">
     <UiButton variant="ghost" class="absolute top-2 right-1">
-      <Icon name="line-md:logout" size="24" />
+      <Icon name="line-md:logout" size="24" @click="handleSignOut" />
     </UiButton>
     <NuxtLink to="/">
       <NuxtImg
