fb21e8064cbc3c57aab280f32ade893853e993a0
LAViswhat <lav.iswhat@gmail.com>
2025-02-11 18:25:11 +0200

    refactor Header component to use UiMenubar and related elements

--- components/layout/navigation/Header.vue
+++ components/layout/navigation/Header.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
 import { useAuthStore } from "~/stores/auth";
 import { useLoaderStore } from "@/stores/loader";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
 
 const authStore = useAuthStore();
 
@@ -22,9 +14,9 @@ const handleSignOut = async () => {
 };
 </script>
 <template>
-  <Menubar>
-    <MenubarMenu>
-      <MenubarTrigger>
+  <UiMenubar>
+    <UiMenubarMenu>
+      <UiMenubarTrigger>
         <div class="flex items-center justify-between gap-1">
           <img src="/public/profile.svg" />
           <div class="flex flex-col items-start text-sm">
@@ -34,24 +26,24 @@ const handleSignOut = async () => {
             }}</span>
           </div>
         </div>
-      </MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
+      </UiMenubarTrigger>
+      <UiMenubarContent>
+        <UiMenubarItem>
           <div class="flex items-center gap-2">
             <Icon name="lucide:settings" size="20" class="text-newblack" />
             <span>Profile settings</span>
           </div>
-        </MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem
+        </UiMenubarItem>
+        <UiMenubarSeparator />
+        <UiMenubarItem
           ><div class="flex items-center gap-2" @click="handleSignOut">
             <Icon name="line-md:logout" size="20" class="text-newblack" />
             <span>Log out</span>
-          </div></MenubarItem
+          </div></UiMenubarItem
         >
-      </MenubarContent>
-    </MenubarMenu>
-  </Menubar>
+      </UiMenubarContent>
+    </UiMenubarMenu>
+  </UiMenubar>
 </template>
 
 <style scoped></style>
