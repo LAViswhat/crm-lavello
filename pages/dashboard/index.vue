75365796af463b357c077259867cd8ed275d70eb
LAViswhat <lav.iswhat@gmail.com>
2025-02-12 16:36:05 +0200

    update dashboard layout

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -5,7 +5,11 @@ definePageMeta({
 });
 </script>
 <template>
-  <div class="p-10">
+  <div class="p-8">
+    <div class="flex items-center gap-1 pb-2">
+      <Icon name="iconamoon:profile-fill" size="32" class="text-primary" />
+      <h3 class="mb-0 normal-case">My boards</h3>
+    </div>
     <LayoutBoardsBoardList />
   </div>
 </template>
