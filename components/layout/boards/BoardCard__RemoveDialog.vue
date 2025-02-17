ba4ecfba4cbc9505ab667d395ea9fe593058f414
LAViswhat <lav.iswhat@gmail.com>
2025-02-17 16:13:44 +0200

    add remove dialog component for board deletion confirmation

---
+++ components/layout/boards/BoardCard__RemoveDialog.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useBoardsStore } from "~/stores/boards";
+
+const boardStore = useBoardsStore();
+const props = defineProps<{
+  boardId: string;
+}>();
+</script>
+<template>
+  <UiAlertDialog>
+    <UiAlertDialogTrigger>
+      <slot name="alertTrigger" />
+    </UiAlertDialogTrigger>
+    <UiAlertDialogContent>
+      <UiAlertDialogHeader>
+        <UiAlertDialogTitle class="normal-case"
+          >Are you absolutely sure?</UiAlertDialogTitle
+        >
+        <UiAlertDialogDescription>
+          This action cannot be undone. This will permanently delete this board
+          and remove your data from our servers.
+        </UiAlertDialogDescription>
+      </UiAlertDialogHeader>
+      <UiAlertDialogFooter>
+        <UiAlertDialogCancel>Cancel</UiAlertDialogCancel>
+        <UiAlertDialogAction @click="boardStore.removeBoard(props.boardId)"
+          >Remove</UiAlertDialogAction
+        >
+      </UiAlertDialogFooter>
+    </UiAlertDialogContent>
+  </UiAlertDialog>
+</template>
+
+<style scoped></style>
