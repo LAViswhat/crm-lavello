91fa8b40b1f6e032d6bbf369320d3568e2c6913e
LAViswhat <lav.iswhat@gmail.com>
2025-05-19 19:38:26 +0300

    simplify board header menu structure

--- components/layout/board/BoardPage__Header.vue
+++ components/layout/board/BoardPage__Header.vue
@@ -26,59 +26,16 @@ const handleRemoveBoard = async () => {
           <div
             class="flex flex-row items-center text-newwhite z-20 duration-200"
           >
-            <h2 class="normal-case mb-0 text-lg sm:text-2xl">
+            <h2 class="normal-case mb-0 text-base sm:text-2xl">
               {{ boardName }}
             </h2>
-            <Icon name="cil:options" size="28" />
+            <Icon name="cil:options" size="20" />
           </div>
         </UiMenubarTrigger>
-        <UiMenubarContent
-          side="right"
-          class="bg-newwhite border-0 w-full flex flex-col"
-        >
-          <UiSheetTrigger as-child>
-            <UiMenubarItem>
-              <div class="flex items-center gap-2">
-                <Icon name="ix:about" size="20" class="text-newblack" />
-                <span>About board</span>
-              </div>
-            </UiMenubarItem>
-          </UiSheetTrigger>
-          <UiMenubarSeparator class="bg-0" />
-          <UiMenubarItem as-child>
-            <LayoutBoardsBoardDialog mode="edit" :board-id="boardId">
-              <template #trigger>
-                <div
-                  class="flex items-center gap-2 w-full cursor-default select-none rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-white focus:bg-gray-100 focus:text-gray-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800 dark:focus:text-gray-50"
-                >
-                  <Icon
-                    name="mdi:text-box-edit-outline"
-                    size="20"
-                    class="text-newblack"
-                  />
-                  <span>Edit board</span>
-                </div>
-              </template>
-            </LayoutBoardsBoardDialog>
-          </UiMenubarItem>
-          <UiMenubarSeparator class="bg-0" />
-          <UiMenubarItem as-child>
-            <LayoutRemoveDialog
-              :board-id="boardId"
-              :description="'This action cannot be undone. This will permanently delete this board and remove your data from our servers.'"
-              :onRemove="handleRemoveBoard"
-            >
-              <template #alertTrigger
-                ><div
-                  class="flex items-center gap-2 w-full cursor-default select-none rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-white focus:bg-gray-100 focus:text-gray-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800 dark:focus:text-gray-50"
-                >
-                  <Icon name="wpf:full-trash" size="20" class="text-newblack" />
-                  <span>Remove board</span>
-                </div>
-              </template>
-            </LayoutRemoveDialog>
-          </UiMenubarItem>
-        </UiMenubarContent>
+        <LayoutBoardPageHeaderMenuContent
+          :board-id="boardId"
+          :handle-remove-board="handleRemoveBoard"
+        />
       </UiMenubarMenu>
     </UiMenubar>
     <LayoutBoardPageHeaderAboutContent
---
+++ components/layout/board/BoardPage__Header__MenuContent.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const props = defineProps<{
+  boardId: string | undefined;
+  handleRemoveBoard: () => Promise<void>;
+}>();
+</script>
+
+<template>
+  <UiMenubarContent
+    side="right"
+    class="bg-newwhite border-0 w-full flex flex-col"
+  >
+    <UiSheetTrigger as-child>
+      <UiMenubarItem>
+        <div class="flex items-center gap-2">
+          <Icon name="ix:about" size="20" class="text-newblack" />
+          <span>About board</span>
+        </div>
+      </UiMenubarItem>
+    </UiSheetTrigger>
+    <UiMenubarSeparator class="bg-0" />
+    <UiMenubarItem as-child>
+      <LayoutBoardsBoardDialog mode="edit" :board-id="boardId">
+        <template #trigger>
+          <div
+            class="flex items-center gap-2 w-full cursor-default select-none rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-white focus:bg-gray-100 focus:text-gray-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800 dark:focus:text-gray-50"
+          >
+            <Icon
+              name="mdi:text-box-edit-outline"
+              size="20"
+              class="text-newblack"
+            />
+            <span>Edit board</span>
+          </div>
+        </template>
+      </LayoutBoardsBoardDialog>
+    </UiMenubarItem>
+    <UiMenubarSeparator class="bg-0" />
+    <UiMenubarItem as-child>
+      <LayoutRemoveDialog
+        :board-id="boardId"
+        :description="'This action cannot be undone. This will permanently delete this board and remove your data from our servers.'"
+        :onRemove="handleRemoveBoard"
+      >
+        <template #alertTrigger>
+          <div
+            class="flex items-center gap-2 w-full cursor-default select-none rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-white focus:bg-gray-100 focus:text-gray-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800 dark:focus:text-gray-50"
+          >
+            <Icon name="wpf:full-trash" size="20" class="text-newblack" />
+            <span>Remove board</span>
+          </div>
+        </template>
+      </LayoutRemoveDialog>
+    </UiMenubarItem>
+  </UiMenubarContent>
+</template>
